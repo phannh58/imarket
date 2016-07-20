6bd2ef6bd275fe22cac8c1bdb1c33239322a2142
linhtn_57 <hotdog160993@gmail.com>
2016-07-20 13:53:43 +0700

    Return store type and stores of a commerce center

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: "json"} do
     scope module: :v1 do
-        resources :stores do
-          resources :categories, only: :index
-        end
+      resources :commerce_centers, only: :show
+      resources :stores do
+        resources :categories, only: :index
+      end
     end
   end
 end
