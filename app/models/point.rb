e5462b33b326eb9f9841d9acbb0c08fc689307ae
Type in your name <Type in your email>
2016-10-20 23:17:03 +0700

    last_check_API_document

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,8 +1,8 @@
 class Point < ActiveRecord::Base
   belongs_to :store_type
-  has_many :active_edges, class_name: Edge.name, foreign_key :point_start_id,
+  has_many :active_edges, class_name: Edge.name, foreign_key: :point_start_id,
     dependent: :destroy
-  has_many :passive_edges, class_name: Edge.name, foreign_key :point_end_id,
+  has_many :passive_edges, class_name: Edge.name, foreign_key: :point_end_id,
     dependent: :destroy
   has_many :matching, through: :active_edges, source: :point_end
   has_many :matchers, through: :passive_edges, source: :point_start
